e263d1db6333fe7753028b03f0fa73ba715313a5
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2018-08-07 15:16:24 +0800

    add

--- TCGA_ID_transform.R
+++ TCGA_ID_transform.R
@@ -1,12 +1,10 @@
-library(GenomicDataCommons)
-manifest <- read.table("G:/biodata/TCGA/gdc_manifest.2018-05-10.txt", header = TRUE, stringsAsFactors = FALSE)
-
-# library(GenomicDataCommons)
-# library(magrittr)
-file_uuids <- manifest$id
-head(file_uuids)
-
+# Source code from biostar and mofied for easier use
 TCGAtranslateID = function(file_ids, legacy = TRUE) {
+    # file_ids: id of download files in TCGA manifest file
+    # legacy: if TRUE, use hg19
+    if(!(require(GenomicDataCommons) & require(magrittr))){
+        install.packages(c("GenomicDataCommons", "magrittr"), dependencies = TRUE)
+    }
     info = files(legacy = legacy) %>%
         filter( ~ file_id %in% file_ids) %>%
         select('cases.samples.submitter_id') %>%
@@ -23,5 +21,9 @@ TCGAtranslateID = function(file_ids, legacy = TRUE) {
                       submitter_id = unlist(id_list)))
 }
 
-res = TCGAtranslateID(file_uuids)
-head(res)
+
+# manifest <- read.table("gdc_manifest_filePath", header = TRUE, stringsAsFactors = FALSE)
+# file_uuids <- manifest$id
+# head(file_uuids)
+# res = TCGAtranslateID(file_uuids)
+# head(res)
